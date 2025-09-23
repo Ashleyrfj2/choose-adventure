f1802a241d6e8c5eb235a1d26cc2b608486789e7
AshleyRFJ2 <AshleyRFJ2@gmail.com>
2025-09-23 15:09:05 -0500

    Ash

---
+++ app.js
@@ -0,0 +1,16 @@
+const prompt = require('prompt-sync')();
+const name = prompt('What is your name? ');
+console.log(` ${name}, it's time for an adventure. Do you want a happy ending or a
+    tragic ending to your adventure?`) ;
+console.log('Type "Happy" or "Tragic" to continue');
+
+
+//Start of path
+const name = require('prompt1-sync')
+
+{
+    if (prompt1 = ) {
+        
+    }
+}
+if 
